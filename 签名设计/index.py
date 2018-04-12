a649e183ca1d7194e61835142072f97397e367e7
2990599771@qq.com <2990599771@qq.com>
2018-04-12 22:43:47 +0800

    添加新工程

---
+++ 签名设计/index.py
@@ -0,0 +1,21 @@
+# _*_coding:utf-8_*_
+# Author:liu
+from tkinter import *
+
+
+def main():
+    # 创建窗口
+    root = Tk()
+    # 标题
+    root.title('签名设计')
+    # 设置窗口大小 宽x高
+    root.geometry('600x300')
+    # 窗口初始位置
+    root.geometry('+500+200')
+
+    # 消息循环
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
