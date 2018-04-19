eff4ae0ce8d5d39b8fd30581b477e3aefdcff17a
2990599771@qq.com <2990599771@qq.com>
2018-04-19 21:39:20 +0800

    添加功能

--- 微信好友分析/微信好友分析.py
+++ 微信好友分析/微信好友分析.py
@@ -23,24 +23,24 @@ def tongji(friend_list):
             sex_dict['female'] += 1
 
 
-# def show_area_distribution(friends):
-#     # 使用一个字典统计各省好友数量
-#     province_dict = {'北京': 0, '上海': 0, '天津': 0, '重庆': 0,
-#                      '河北': 0, '山西': 0, '吉林': 0, '辽宁': 0, '黑龙江': 0,
-#                      '陕西': 0, '甘肃': 0, '青海': 0, '山东': 0, '福建': 0,
-#                      '浙江': 0, '台湾': 0, '河南': 0, '湖北': 0, '湖南': 0,
-#                      '江西': 0, '江苏': 0, '安徽': 0, '广东': 0, '海南': 0,
-#                      '四川': 0, '贵州': 0, '云南': 0,
-#                      '内蒙古': 0, '新疆': 0, '宁夏': 0, '广西': 0, '西藏': 0,
-#                      '香港': 0, '澳门': 0}
+def show_area_distribution(friends):
+    # 使用一个字典统计各省好友数量
+    province_dict = {'北京': 0, '上海': 0, '天津': 0, '重庆': 0,
+                     '河北': 0, '山西': 0, '吉林': 0, '辽宁': 0, '黑龙江': 0,
+                     '陕西': 0, '甘肃': 0, '青海': 0, '山东': 0, '福建': 0,
+                     '浙江': 0, '台湾': 0, '河南': 0, '湖北': 0, '湖南': 0,
+                     '江西': 0, '江苏': 0, '安徽': 0, '广东': 0, '海南': 0,
+                     '四川': 0, '贵州': 0, '云南': 0,
+                     '内蒙古': 0, '新疆': 0, '宁夏': 0, '广西': 0, '西藏': 0,
+                     '香港': 0, '澳门': 0}
 
-#     # 统计省份
-#     for friend in friends:
-#         if friend.province in province_dict.keys():
-#             province_dict[friend.province] += 1
-#     # {'山西': 7, '上海': 0, '台湾': 0, '青海': 0,...}
+    # 统计省份
+    for friend in friends:
+        if friend.province in province_dict.keys():
+            province_dict[friend.province] += 1
+    # {'山西': 7, '上海': 0, '台湾': 0, '青海': 0,...}
 
-#     print(province_dict)
+    print(province_dict)
 
 
 def main():
