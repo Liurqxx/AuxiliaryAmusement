859c568ab6950f9ebf901fd92bb621b8e65ff4b9
2990599771@qq.com <2990599771@qq.com>
2018-04-24 22:35:35 +0800

    添加功能

--- 微信数据查看/微信数据查看.py
+++ 微信数据查看/微信数据查看.py
@@ -39,52 +39,7 @@ def handle_receive_msg(msg):
 @itchat.msg_register(NOTE, isFrien
             msg_content = r"纬度->" + x.__str__() + " 经度->" + y.__str__()  # 内容为详细的地址
         else:
-            msg_content = r"" + location
-    elif msg['Type'] == 'Sharing':  # 如果消息为分享的音乐或者文章，详细的内容为文章的标题或者是分享的名字
-        msg_content = msg['Text']
-        msg_share_url = msg['Url']  # 记录分享的url
-        print(msg_share_url)
-
-    face_bug = msg_content
-
-    # 将信息存储在字典中，每一个msg_id对应一条信息
-    msg_information.update(
-        {
-            msg_id: {
-                "msg_from": msg_from, "msg_time": msg_time, "msg_time_rec": msg_time_rec,
-                "msg_type": msg["Type"],
-                "msg_content": msg_content, "msg_share_url": msg_share_url
-            }
-        }
-    )
-
-
-# 这个是用于监听是否有Group消息撤回
-@itchat.msg_register(NOTE, isGroupChat=True, isMpChat=True)
-def information(msg):
-    # 这里如果这里的msg['Content']中包含消息撤回和id，就执行下面的语句
-    if '撤回了一条消息' in msg['Content']:
-        old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)  # 在返回的content查找撤回的消息的id
-        old_msg = msg_information.get(old_msg_id)  # 得到消息
-        print(old_msg)
-
-        if len(old_msg_id) < 11:  # 如果发送的是表情包
-            itchat.send_file(face_bug, toUserName='filehelper')
-        else:  # 发送撤回的提示给文件助手
-            msg_body = "【" \
-                       + old_msg.get('msg_from') + " 群消息撤回提醒】\n" \
-                       + " 撤回了 " + old_msg.get("msg_type") + " 消息：" + "\n" \
-                       + old_msg.get('msg_time_rec') + "\n" \
-                       + r"" + old_msg.get('msg_content')
-            # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
-            if old_msg['msg_type'] == "Sharing":
-                msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
-
-            # 将撤回消息发送到文件助手
-            itchat.send_msg(msg_body, toUserName='filehelper')
-            # 有文件的话也要将文件发送回去
-            if old_msg["msg_type"] == "Picture" \
-                    or old_msg["msg_type"] == "Recording" \
+   old_msg["msg_type"] == "Recording" \
                     or old_msg["msg_type"] == "Video" \
                     or old_msg["msg_type"] == "Attachment":
                 file = '@fil@%s' % (old_msg['msg_content'])
@@ -97,5 +52,6 @@ def information(msg):
 if __name__ == '__main__':
     # Main
     # itchat.auto_login(enableCmdQR=True, hotReload=True)
+    #登录微信
     itchat.auto_login()
     itchat.run()
