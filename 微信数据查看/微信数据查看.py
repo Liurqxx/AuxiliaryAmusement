f42d4291d3bdb803b082d0170e862d2dbf3be017
2990599771@qq.com <2990599771@qq.com>
2018-04-27 22:21:36 +0800

    修改

--- 微信数据查看/微信数据查看.py
+++ 微信数据查看/微信数据查看.py
@@ -11,39 +11,6 @@ import os
 
 
 
-# 这个是用于监听是否有friend消息撤回
-@itchat.msg_register(NOTE, isFriendChat=True, isGroupChat=True, isMpChat=True)
-def information(msg):
-    # 这里如果这里的msg['Content']中包含消息撤回和id，就执行下面的语句
-    if '撤回了一条消息' in msg['Content']:
-        old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)  # 在返回的content查找撤回的消息的id
-        old_msg = msg_information.get(old_msg_id)  # 得到消息
-        print(old_msg)
-
-        if len(old_msg_id) < 11:  # 如果发送的是表情包
-            itchat.send_file(face_bug, toUserName='filehelper')
-        else:  # 发送撤回的提示给文件助手
-            msg_body = "[" \
-                       + old_msg.get('msg_from') + " 撤回了 ]\n" \
-                       + old_msg.get("msg_type") + " 消息：" + "\n" \
-                       + old_msg.get('msg_time_rec') + "\n" \
-                       + r"" + old_msg.get('msg_content')
-            # 如果是分享的文件被撤回了，那么就将分享的url加在msg_body中发送给文件助手
-            if old_msg['msg_type'] == "Sharing":
-                msg_body += "\n就是这个链接➣ " + old_msg.get('msg_share_url')
-
-            # 将撤回消息发送到文件助手
-            itchat.send_msg(msg_body, toUserName='filehelper')
-            # 有文件的话也要将文件发送回去
-            if old_msg["msg_type"] == "Picture" \
-                    or old_msg["msg_type"] == "Recording" \
-                    or old_msg["msg_type"] == "Video" \
-                    or old_msg["msg_type"] == "Attachment":
-                file = '@fil@%s' % (old_msg['msg_content'])
-                itchat.send(msg=file, toUserName='filehelper')
-                os.remove(old_msg['msg_content'])
-            # 删除字典旧消息
-            msg_information.pop(old_msg_id)
 
 
 @itchat.msg_register([TEXT, PICTURE, FRIENDS, CARD, MAP, SHARING, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
