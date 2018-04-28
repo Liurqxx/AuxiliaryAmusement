0f6e15f016a4b0a496af44eef9a6e2526d6a078a
2990599771@qq.com <2990599771@qq.com>
2018-04-28 22:04:40 +0800

    添加工程

---
+++ PDF转word文字/Pdf转word.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+
+'''
+    pdf->word
+    整体思路：构造文档对象，解析文档对象，提取所需内容
+'''
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+    main()
