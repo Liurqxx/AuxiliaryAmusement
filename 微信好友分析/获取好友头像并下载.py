ac805d9756cdcb8f7256acca9185d5172f6ce50b
2990599771@qq.com <2990599771@qq.com>
2018-04-30 13:24:44 +0800

    添加功能文件

---
+++ 微信好友分析/获取好友头像并下载.py
@@ -0,0 +1,39 @@
+# _*_coding:utf-8_*_
+# Author:liu
+from wxpy import Bot
+from PIL import Image
+import math
+import os
+import shutil
+import sys
+
+'''
+下载微信好友的头像并拼接所有的图片
+'''
+
+
+def down_img(friends):
+    '''下载头像'''
+
+
+def show_img():
+    '''借助plt的Image库进行头像拼接'''
+   
+
+
+def main():
+    # 登录微信
+    # bot = Bot(cache_path=True)
+
+    # 获取好友列表
+    # friends = bot.friends()
+
+    # 下载好友头像
+    # down_img(friends)
+
+    # 拼接好友头像
+    # show_img()
+
+
+if __name__ == '__main__':
+    main()
