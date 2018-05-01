27f3f83874a06f5b3a408a003d8a114b6628e061
2990599771@qq.com <2990599771@qq.com>
2018-05-01 20:39:22 +0800

    添加工程

---
+++ 图片字符/图片字符.py
@@ -0,0 +1,25 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+from PIL import Image
+
+
+
+# 变量ascii_char: 存储用于显示图片的字符种类。我们要注意到，这个list的最后一个元素是空格，
+# 这表示，我们 # 将使用空格来代替原图片中灰度值最高的像素点（在灰度图像中，灰度值最高为255，代表白色，最低为0，代表黑色）。
+#  list中的第一个元素是$，这表示我们将使用$来代替原图片中灰度值最低的像素点。其余字符依此类推。
+
+
+
+
+# ＃ 把RGB转为灰度值，并且返回该灰度值对应的字符标记
+def select_ascii_char(r, g, b):
+
+
+
+def output(imgpath, width=100, height=100):
+
+
+def save_as_txtfile(txt):
+
+if __name__ == '__main__':
+    
