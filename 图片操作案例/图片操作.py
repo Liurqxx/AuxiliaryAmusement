b068f0fec491f89bccca21818d644f6edf348f8f
2990599771@qq.com <2990599771@qq.com>
2018-05-05 22:34:38 +0800

    添加文件

---
+++ 图片操作案例/图片操作.py
@@ -0,0 +1,42 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+
+'''
+    把一张图片切成9张图片
+'''
+from PIL import Image
+import sys
+
+
+# 将图片填充为正方形
+def fill_image(image):
+    width, height = image.size
+    # 选取长和宽中较大值作为新图片的
+    new_image_length = width if width > height else height
+    # 生成新图片[白底]
+    new_image = Image.new(image.mode, (new_image_length, new_image_length), color='white')
+    # 将之前的图粘贴在新图上，居中
+    if width > height:  # 原图宽大于高，则填充图片的竖直维度
+        new_image.paste(image, (0, int((new_image_length - height) / 2)))  # (x,y)二元组表示粘贴上图相对下图的起始位置
+    else:
+        new_image.paste(image, (int((new_image_length - width) / 2), 0))
+    return new_image
+
+
+
+# 保存
+def save_images(image_list):
+    index = 1
+    for image in image_list:
+        image.save('./result/python' + str(index) + '.png', 'PNG')
+        index += 1
+
+
+if __name__ == '__main__':
+    file_path = "img.jpg"
+    image = Image.open(file_path)
+    # image.show()
+    image = fill_image(image)
+    # image.show()
+    image_list = cut_image(image)
+    save_images(image_list)
