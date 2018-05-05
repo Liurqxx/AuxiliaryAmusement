edcbb10ba4dfbdf4b3962f2d16d77150c7f2fe22
2990599771@qq.com <2990599771@qq.com>
2018-05-05 22:31:47 +0800

    完整代码

--- turtle绘制图案/绘制钟表.py
+++ turtle绘制图案/绘制钟表.py
@@ -72,6 +72,34 @@ def Init():
     printer.penup()
 
 
+def SetupClock(radius):
+    # 建立表的外框
+    turtle.reset()
+    turtle.pensize(7)
+    for i in range(60):
+        Skip(radius)
+        if i % 5 == 0:
+            turtle.forward(20)
+            Skip(-radius - 20)
+
+            Skip(radius + 20)
+            if i == 0:
+                turtle.write(int(12), align="center", font=("Courier", 14, "bold"))
+            elif i == 30:
+                Skip(25)
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+                Skip(-25)
+            elif (i == 25 or i == 35):
+                Skip(20)
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+                Skip(-20)
+            else:
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+            Skip(-radius - 20)
+        else:
+            turtle.dot(5)
+            Skip(-radius)
+        turtle.right(6)
 
 
 def Week(t):
@@ -80,7 +108,45 @@ def Week(t):
     return week[t.weekday()]
 
 
+def Date(t):
+    y = t.year
+    m = t.month
+    d = t.day
+    return "%s %d%d" % (y, m, d)
 
+
+def Tick():
+    # 绘制表针的动态显示
+    t = datetime.today()
+    second = t.second + t.microsecond * 0.000001
+    minute = t.minute + second / 60.0
+    hour = t.hour + minute / 60.0
+    secHand.setheading(6 * second)
+    minHand.setheading(6 * minute)
+    hurHand.setheading(30 * hour)
+
+    turtle.tracer(False)
+    printer.forward(65)
+    printer.write(Week(t), align="center",
+                  font=("Courier", 14, "bold"))
+    printer.back(130)
+    printer.write(Date(t), align="center",
+                  font=("Courier", 14, "bold"))
+    printer.home()
+    turtle.tracer(True)
+
+    # 100ms后继续调用tick
+    turtle.ontimer(Tick, 100)
+
+
+def main():
+    # 打开/关闭龟动画，并为更新图纸设置延迟。
+    turtle.tracer(False)
+    Init()
+    SetupClock(160)
+    turtle.tracer(True)
+    Tick()
+    turtle.mainloop()
 
 
 if __name__ == "__main__":
