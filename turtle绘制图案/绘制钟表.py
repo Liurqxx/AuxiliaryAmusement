06c882b259f90a508f753e15f135a890338c7e70
2990599771@qq.com <2990599771@qq.com>
2018-05-05 22:30:18 +0800

    添加文件

---
+++ turtle绘制图案/绘制钟表.py
@@ -0,0 +1,115 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+
+
+import turtle
+from datetime import *
+
+'''
+    datetime模块：
+        datetime模块用于是date和time模块的合集，datetime有两个常量，MAXYEAR和MINYEAR，分别是9999和1.
+
+        datetime模块定义了5个类，分别是:
+
+                1.datetime.date：表示日期的类
+
+                2.datetime.datetime：表示日期时间的类
+
+                3.datetime.time：表示时间的类
+
+                4.datetime.timedelta：表示时间间隔，即两个时间点的间隔
+
+                5.datetime.tzinfo：时区的相关信息
+'''
+
+
+# 抬起画笔，向前运动一段距离放下
+def Skip(step):
+    turtle.penup()
+    turtle.forward(step)
+    turtle.pendown()
+
+
+def mkHand(name, length):
+    # 注册Turtle形状，建立表针Turtle
+    turtle.reset()
+    Skip(-length * 0.1)
+    # 开始记录多边形的顶点。当前的乌龟位置是多边形的第一个顶点。
+    turtle.begin_poly()
+    turtle.forward(length * 1.1)
+    # 停止记录多边形的顶点。当前的乌龟位置是多边形的最后一个顶点。将与第一个顶点相连。
+    turtle.end_poly()
+    # 返回最后记录的多边形。
+    handForm = turtle.get_poly()
+    turtle.register_shape(name, handForm)
+
+
+def Init():
+    global secHand, minHand, hurHand, printer
+    # 重置Turtle指向北
+    turtle.mode("logo")
+
+    # 建立三个表针Turtle并初始化
+    mkHand("secHand", 135)
+    mkHand("minHand", 125)
+    mkHand("hurHand", 90)
+
+    secHand = turtle.Turtle()
+    secHand.shape("secHand")
+    minHand = turtle.Turtle()
+    minHand.shape("minHand")
+    hurHand = turtle.Turtle()
+    hurHand.shape("hurHand")
+
+    for hand in secHand, minHand, hurHand:
+        hand.shapesize(1, 1, 3)
+        hand.speed(0)
+
+        # 建立输出文字Turtle
+    printer = turtle.Turtle()
+    # 隐藏画笔的turtle形状
+    printer.hideturtle()
+    printer.penup()
+
+
+def SetupClock(radius):
+    # 建立表的外框
+    turtle.reset()
+    turtle.pensize(7)
+    for i in range(60):
+        Skip(radius)
+        if i % 5 == 0:
+            turtle.forward(20)
+            Skip(-radius - 20)
+
+            Skip(radius + 20)
+            if i == 0:
+                turtle.write(int(12), align="center", font=("Courier", 14, "bold"))
+            elif i == 30:
+                Skip(25)
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+                Skip(-25)
+            elif (i == 25 or i == 35):
+                Skip(20)
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+                Skip(-20)
+            else:
+                turtle.write(int(i / 5), align="center", font=("Courier", 14, "bold"))
+            Skip(-radius - 20)
+        else:
+            turtle.dot(5)
+            Skip(-radius)
+        turtle.right(6)
+
+
+def Week(t):
+    week = ["星期一", "星期二", "星期三",
+            "星期四", "星期五", "星期六", "星期日"]
+    return week[t.weekday()]
+
+
+
+
+
+if __name__ == "__main__":
+    main()
