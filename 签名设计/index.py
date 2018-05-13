b26258937efedf1be7d1a40aa90575e02d55c21d
2990599771@qq.com <2990599771@qq.com>
2018-05-13 22:50:18 +0800

    修改文件

--- 签名设计/index.py
+++ 签名设计/index.py
@@ -74,29 +74,29 @@ def download():
         label2.grid(row=2, columnspan=2)
 
 
-if __name__ == '__main__':
-    # 创建窗口
-    root = Tk()
-    # 标题
-    root.title('签名设计')
-    # 设置窗口大小 宽x高
-    root.geometry('600x300')
-    # 窗口初始位置
-    root.geometry('+500+200')
 
-    # 设置标签 root:所在窗口
-    label = Label(root, text='签名', font=('华文行楷', 20), fg='red')
-    # 定位方式:grid pack place
-    # 显示在第0行第0列
-    label.grid()  # 等价于：label.grid(row=0, column=0)
-
-    # 输入框
-    entry = Entry(root, font=('微软雅黑', 20))
     # 显示在第0行第1列
     entry.grid(row=0, column=1)
     # 程序运行时光标停在此处
     entry.focus()
 
+    # 定义点击按钮 command:触发事件
+    button = Button(root, text='设计签名', font=('微软雅黑', 20), command=download)
+    # 定位
+    button.grid(row=1, column=0)
+
+    # 下拉菜单
+    # 创建一个下拉列表
+    font_list = StringVar()
+    # state:把菜单设置为只读模式
+    fontChosen = ttk.Combobox(root, width=12, textvariable=font_list, font=('微软雅黑', 20), state='readonly')
+
+    # 设置下拉列表的值
+    fontChosen['values'] = ('个性签', '连笔签', '潇洒签', '草体签', '合文签', '商务签', '可爱签')
+    # 设置其在界面中出现的位置  column代表列   row 代表行
+    fontChosen.grid(column=1, row=1)
+    # 设置下拉列表默认显示的值，0为 fontChosen['values'] 的下标值
+    fontChosen.current(0)
 
     # 消息循环
     root.mainloop()
