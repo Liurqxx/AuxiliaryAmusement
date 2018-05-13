eb9a93fbbe93f49dfe51dbf0fb450950ab648469
2990599771@qq.com <2990599771@qq.com>
2018-05-13 22:51:51 +0800

    修改文件

--- PDF转word文字/Pdf转word.py
+++ PDF转word文字/Pdf转word.py
@@ -11,29 +11,27 @@ from pdfminer.converter import PDFPageAggregator
 '''
 
 
+def main():
+    # 打开本地pdf文件
+    file = open('./文档.pdf', 'rb')
+
+    # 创建一个pdf文档分析器
+    parser = PDFParser(file)
+
+    # 创建一个pdf文档
+    doc = PDFDocument()
+
+    # 链接分析器和文档对象
+    parser.set_document(doc)
+    doc.set_parser(parser)
+
+    # 初始化
+    doc.initialize('')
 
     # 检测文档是否提供txt转换，不提供就忽略
     if not doc.is_extractable:
         raise Exception
-
-    else:
-        # 创建PDF资源管理器
-        resource = PDFResourceManager()
-
-        # 创建一个PDF参数分析器
-        laparams = LAParams()
-
-        # 创建聚合器，用于读取文档的对象
-        device = PDFPageAggregator(resource, laparams=laparams)
-
-        # 创建解释器，对文档编码，解释成Python能够识别的格式
-        interpreter = PDFPageInterpreter(resource, device)
-
-        # 循环遍历列表，每次处理一页的内容
-        # doc.get_pages() 获取page列表
-        for page in doc.get_pages():
-            # 利用解释器的process_page()方法解析读取单独页数
-            interpreter.process_page(page)
+     interpreter.process_page(page)
             # 使用聚合器get_result() 方法获取内容
             layout = device.get_result()
             # 这里layout是一个LTPage对象
