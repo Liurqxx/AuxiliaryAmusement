c0ea7cc9e094d2ba0857a051cdfaaf3850d81d2d
2990599771@qq.com <2990599771@qq.com>
2018-05-13 22:46:27 +0800

    修改代码

--- 图片操作案例/图片一分九.py
+++ 图片操作案例/图片一分九.py
@@ -8,20 +8,20 @@ from PIL import Image
 import sys
 
 
-def cut_image(image):
+# 将图片填充为正方形
+def fill_image(image):
     width, height = image.size
-    item_width = int(width / 3)
-    box_list = []
-    # (left, upper, right, lower)
-    for i in range(0, 3):
-        for j in range(0, 3):
-            # print((i*item_width,j*item_width,(i+1)*item_width,(j+1)*item_width))
-            box = (j * item_width, i * item_width, (j + 1) * item_width, (i + 1) * item_width)
-            box_list.append(box)
+    # 选取长和宽中较大值作为新图片的
+    new_image_length = width if width > height else height
+    # 生成新图片[白底]
+    new_image = Image.new(image.mode, (new_image_length, new_image_length), color='white')
+    # 将之前的图粘贴在新图上，居中
+    if width > height:  # 原图宽大于高，则填充图片的竖直维度
+        new_image.paste(image, (0, int((new_image_length - height) / 2)))  # (x,y)二元组表示粘贴上图相对下图的起始位置
+    else:
+        new_image.paste(image, (int((new_image_length - width) / 2), 0))
+    return new_image
 
-    image_list = [image.crop(box) for box in box_list]
-
-    return image_list
 
 
 # 保存
