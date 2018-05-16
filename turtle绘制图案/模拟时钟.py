9ea4e73221764a300ee5a7e09f1822bed578de4a
2990599771@qq.com <2990599771@qq.com>
2018-05-16 23:04:07 +0800

    修改文件

--- turtle绘制图案/模拟时钟.py
+++ turtle绘制图案/模拟时钟.py
@@ -22,18 +22,33 @@ pygame.init()
 screen = pygame.display.set_mode((600, 500))
 pygame.display.set_caption("CLOCK")
 font = pygame.font.Font(None, 36)
-orange = 220, 180, 0
-white = 255, 255, 255
-yellow = 255, 255, 0
-pink = 255, 100, 100
+orang
+    # draw the clock number 1-12
+    for n in range(1, 13):
+        angle = math.radians(n * (360 / 12) - 90)
+        x = math.cos(angle) * (radius - 20) - 10
+        y = math.sin(angle) * (radius - 20) - 10
+        print_text(font, pos_x + x, pos_y + y, str(n))
 
-pos_x = 300
-pos_y = 250
-radius = 250
-angle = 360
+    # get the time of day
+    today = datetime.today()
+    hours = today.hour % 12
+    minutes = today.minute
+    seconds = today.second
 
-# repeating loop
-in_y = math.sin(min_angle) * (radius - 60)
+    # draw the hours hand
+    hour_angle = wrap_angle(hours * (360 / 12) - 90)
+    hour_angle = math.radians(hour_angle)
+    hour_x = math.cos(hour_angle) * (radius - 80)
+    hour_y = math.sin(hour_angle) * (radius - 80)
+    target = (pos_x + hour_x, pos_y + hour_y)
+    pygame.draw.line(screen, pink, (pos_x, pos_y), target, 12)
+
+    # draw the minutes hand
+    min_angle = wrap_angle(minutes * (360 / 60) - 90)
+    min_angle = math.radians(min_angle)
+    min_x = math.cos(min_angle) * (radius - 60)
+    min_y = math.sin(min_angle) * (radius - 60)
     target = (pos_x + min_x, pos_y + min_y)
     pygame.draw.line(screen, orange, (pos_x, pos_y), target, 12)
 
