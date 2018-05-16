cfc21d8fc0faa9a48fa70beb10f27fca6de7f559
2990599771@qq.com <2990599771@qq.com>
2018-05-16 23:06:13 +0800

    修改文件

--- turtle绘制图案/指定文字画心形.py
+++ turtle绘制图案/指定文字画心形.py
@@ -12,4 +12,12 @@ for item in words.split():
         list_X = []  # list_X是X轴上的打印字符列表，里面装着一个String类的letters
         letters = ''  # letters即为list_X内的字符串，实际是本行要打印的所有字符
         for x in range(-30, 30):  # *是乘法，**是幂次方
-            expression = ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 
+            expression = ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3
+            if expression <= 0:
+                letters += item[(x - y) % len(item)]
+            else:
+                letters += ' '
+        list_X.append(letters)
+        letterlist += list_X
+    print('\n'.join(letterlist))
+    time.sleep(1.5);
