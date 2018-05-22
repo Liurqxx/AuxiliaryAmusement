2f1ec763008ccbc51157b10cff02c71bdff43542
2990599771@qq.com <2990599771@qq.com>
2018-05-22 22:39:57 +0800

    添加代码

---
+++ python发送邮箱/send_email.py
@@ -0,0 +1,9 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+
