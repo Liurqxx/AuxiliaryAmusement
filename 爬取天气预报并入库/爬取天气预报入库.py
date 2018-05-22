5f32077f26bcc59c9e8aeb435f881a814b1d7cb0
2990599771@qq.com <2990599771@qq.com>
2018-05-22 22:37:49 +0800

    完整代码

--- 爬取天气预报并入库/爬取天气预报入库.py
+++ 爬取天气预报并入库/爬取天气预报入库.py
@@ -6,7 +6,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 class SearchWeather():
     def __init__(self):
         self.HEADERS = {
@@ -24,27 +23,19 @@ class SearchWeather():
                 return result['cityCode']
         except Exception as e:
             print(repr(e))
-    weather = "日期      天气    【温度】    风向风力\n"
+
+    def getWeather(self, cityCode, cityname):
+        url = 'http://www.weather.com.cn/weather/%s.shtml' % cityCode
+        html = requests.get(url, headers=self.HEADERS)
+        html.encoding = 'utf-8'
+        soup = BeautifulSoup(html.text, 'lxml')
+        weather = "日期      天气    【温度】    风向风力\n"
         for item in soup.find("div", {'id': '7d'}).find('ul').find_all('li'):
             date, detail = item.find('h1').string, item.find_all('p')
             title = detail[0].string
             templow = detail[1].find("i").string
             temphigh = detail[1].find('span').string if detail[1].find('span') else ''
             wind, direction = detail[2].find('span')['title'], detail[2].find('i').string
-            if temphigh == '':
-                weather += '你好，【%s】今天白天：【%s】，温度：【%s】，%s：【%s】\n' % (cityname, title, templow, wind, direction)
-            else:
-                weather += (date + title + "【" + templow + "~" + temphigh + '°C】' + wind + direction + "\n")
-        return weather
-
-    def main(self, city):
-        cityCode = self.getcityCode(city)
-        detail = self.getWeather(cityCode, city)
-        print(detail)
-
-
-if __name__ == "__main__":
-    weather = SearchWeather()
             if temphigh == '':
                 weather += '你好，【%s】今天白天：【%s】，温度：【%s】，%s：【%s】\n' % (cityname, title, templow, wind, direction)
             else:
