1ddc58358677f512512ff57abcb4eeae545afe4f
2990599771@qq.com <2990599771@qq.com>
2018-05-22 22:35:49 +0800

    添加代码

--- 爬取天气预报并入库/爬取天气预报入库.py
+++ 爬取天气预报并入库/爬取天气预报入库.py
@@ -6,3 +6,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
+class SearchWeather():
+    def __init__(self):
+        self.HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 ''(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+        self.CONNECTION = pymysql.connect(host='localhost', user='root', password='xxx', db='xxx', charset='utf8',
+                                          cursorclass=pymysql.cursors.DictCursor)
+
+    def getcityCode(self, cityName):
+        SQL = "SELECT cityCode FROM cityWeather WHERE cityName='%s'" % cityName
+        try:
+            with self.CONNECTION.cursor() as cursor:
+                cursor.execute(SQL)
+                self.CONNECTION.commit()
+                result = cursor.fetchone()
+                return result['cityCode']
+        except Exception as e:
+            print(repr(e))
