c686b9cb738798dfee343583f94f69a60558b9dc
2990599771@qq.com <2990599771@qq.com>
2018-05-22 22:42:09 +0800

    最后代码

--- python发送邮箱/send_email.py
+++ python发送邮箱/send_email.py
@@ -6,15 +6,17 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
+# sender是邮件发送人邮箱，passWord是服务器授权码，mail_host是服务器地址（这里是QQsmtp服务器）
+sender = '***@qq.com'  #
+passWord = '*******'
+mail_host = 'smtp.qq.com'
+# receivers是邮件接收人，用列表保存，可以添加多个
+receivers = ['121116268@qq.com', 'bbb@163.com']
 
-
-
-
-
-
-
-
-sg['Subject'] = input(u'请输入邮件主题：')
+# 设置email信息
+msg = MIMEMultipart()
+# 邮件主题
+msg['Subject'] = input(u'请输入邮件主题：')
 # 发送方信息
 msg['From'] = sender
 # 邮件正文是MIMEText:
@@ -22,7 +24,22 @@ msg_content = input(u'请输入邮件主内容:')
 msg.attach(MIMEText(msg_content, 'plain', 'utf-8'))
 # 添加附件就是加上一个MIMEBase，从本地读取一个图片:
 with open(u'./img.png', 'rb') as f:
-    # 设置
+    # 设置附件的MIME和文件名，这里是jpg类型,可以换png或其他类型:
+    mime = MIMEBase('image', 'jpg', filename='Lyon.png')
+    # 加上必要的头信息:
+    mime.add_header('Content-Disposition', 'attachment', filename='Lyon.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    # 把附件的内容读进来:
+    mime.set_payload(f.read())
+    # 用Base64编码:
+    encoders.encode_base64(mime)
+    # 添加到MIMEMultipart:
+    msg.attach(mime)
+
+# 登录并发送邮件
+try:
+    # QQsmtp服务器的端口号为465或587
     s = smtplib.SMTP_SSL("smtp.qq.com", 465)
     s.set_debuglevel(1)
     s.login(sender, passWord)
